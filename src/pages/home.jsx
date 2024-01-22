e3084136103175a1404bae68477c18211156f267
Ankit Gupta <iambittuankitbkg@gmail.com>
2024-01-22 22:57:30 +0530

    homepage theme black only development and styling done

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import Navbar from "../components/navbar";
 import "../styles/home.css";
+import myImage from "../assets/images/ankit.png";
+import { FaGithub, FaInstagram, FaLinkedin, FaTwitter } from "react-icons/fa";
 
 const Home = (props) => {
   return (
@@ -13,9 +15,25 @@ const Home = (props) => {
         />
       </header>
       <main className="home-main">
-        <div className="circle"></div>
-        <h1>Ankit</h1>
+        <div className="main-items">
+          <div className="left">
+            <h1 className="firstname">Ankit</h1>
+            <div className="btn">
+              <button>Hire</button>
+              <button>Contact</button>
+            </div>
+          </div>
+          <img src={myImage} alt="ankit-image" />
+          <h1 className="lastname">Gupta</h1>
+        </div>
+        <div className="footer">
+          <FaLinkedin className="icon" />
+          <FaGithub className="icon" />
+          <FaTwitter className="icon" />
+          <FaInstagram className="icon" />
+        </div>
       </main>
+      <div className="circle"></div>
     </div>
   );
 };
