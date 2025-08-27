d2066b30be1c560ac3e4a99c35f911f80dbd896d
Ankit Gupta <iambittuankitbkg@gmail.com>
2025-08-27 17:24:03 +0530

    navbar navigation added and home page image included.

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,31 @@
 import Navbar from "../components/Navbar";
+import boy from "../assets/img/boy.png";
 
 const Home = () => {
   return (
     <>
       <Navbar />
-      <div className="container">This is Home Page.</div>
+      <main className="home-main">
+        <div className="circle"></div>
+        <div className="home container">
+          <div className="left">
+            <h1>Ankit Gupta</h1>
+            <p>A Passionate Software Developer.</p>
+          </div>
+          <div className="right">
+            <img
+              style={{
+                width: "100%",
+                height: "80%",
+                position: "relative",
+                top: "5rem",
+              }}
+              src={boy}
+              alt="boy-holding-laptop"
+            />
+          </div>
+        </div>
+      </main>
     </>
   );
 };
